a86642aebe30332f33cb2a516d872aa71bce4d75
András Ács <acsandras@gmail.com>
2019-12-13 14:52:50 +0100

    Opstartskode

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Developed by András Ács (acsandras@gmail.com)
+ * Zealand / www.zealand.dk
+ * Licensed under the MIT License
+ * 13/12/2019
+ */
+
+public class Main {
+
+    // Your code here 
+
+}
