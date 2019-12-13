e04914253a373c19f1ab2781cf2ef567d6f0c561
András Ács <acsandras@gmail.com>
2019-12-13 14:43:04 +0100

    Starter

---
+++ test/MainTest.java
@@ -0,0 +1,20 @@
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+
+    public void testHelloAndras() {
+
+        Main m = new Main();
+
+        assertEquals("Hello András!", m.hello("András"));
+
+    }
+
+
+    public void testHelloEmptyString() {
+
+        Main m = new Main();
+
+        assertEquals("Hello World!", m.hello(""));
+    }
+}
