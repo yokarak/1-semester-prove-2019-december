da96424e29922262e5c9d4b11743b27a0a9c7a1c
yokarak <ande67f3@edu.easj.dk>
2019-12-20 10:01:15 +0100

    lavede opgave 4

--- src/Loops.java
+++ src/Loops.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 /**
  * Developed by András Ács (acsandras@gmail.com)
  * Zealand / www.zealand.dk
@@ -20,28 +24,39 @@ public class Loops {
         l.printBrugereReverseArr();
         System.out.println("*******");
         l.waitingFor0();
-    }
+    } // jeg var usikker på om jeg skulle blive ved med at printe brugerne, for hver gang der blev gættet
+    //men det var ikke sådan på billedet så lod vær
 
     /**
      * Viser en liste med brugere i konsollen
      */
     public void printBrugereArr() {
 
-        // TODO Din kode her
+        String brugere[] = {"Anders ", "Birgit ", "Carsten ", "Dennis ", "Esben "};
+        for (int i =0; i<brugere.length; i++) {
+            System.out.print(brugere[i]);
+        }
+        System.out.println("");
     }
-
     /**
      * Viser en liste med brugere i konsollen i omvendt rækkefølge
      */
     public void printBrugereReverseArr() {
 
-        // TODO Din kode her
+        Collections.reverse(Arrays.asList(brugere));
+        System.out.println(Arrays.asList(brugere));
 
     }
 
     public void waitingFor0() {
-
-        // TODO Din kode her
- 
+        Scanner input = new Scanner(System.in);
+        int number = 1;
+        while (number != 0) {
+            System.out.print("Venter på 0! Skriv et tal... " );
+            number =input.nextInt();
+        }
+        if (number==0) {
+            System.out.println("Du har tastet 0. Så er det slut!");
+        }
     }
 }
