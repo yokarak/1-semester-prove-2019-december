6654d7ab3a643182be6aa8079accd5993aa69e94
yokarak <ande67f3@edu.easj.dk>
2019-12-20 08:57:57 +0100

    lavede opgave 2 tilføjede lidt kommentarer

--- test/MainTest.java
+++ test/MainTest.java
@@ -5,17 +5,20 @@ public class MainTest extends TestCase {
 
     public void testHelloAndras() {
 
-        Main m = new Main();
+        Main m = new Main(); // man kan fjerne dette grundet af at jeg har gjort hello static, lader det være grundet af at
+                            // det virker forkert at fjerne det, eftersom du har skrevet det :/
 
-        assertEquals("Hello András!", m.hello("András"));
+        assertEquals("Hello András", m.hello("András"));
+        //fyfy, de skal være ens, der må ikke være "!" ekstra i testen :D
 
     }
-
+            //var det virkeligt kun at fjerne "!" ?,
+            // ville være mere korrekt hvis de ikke var der i forvejen :/
 
     public void testHelloEmptyString() {
 
         Main m = new Main();
 
-        assertEquals("Hello World!", m.hello(""));
+        assertEquals("Hello World", m.hello(""));
     }
 }
