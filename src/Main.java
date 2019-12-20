ec8d07af3a898ce5b6b8ef82465e970a03c57e05
yokarak <ande67f3@edu.easj.dk>
2019-12-20 08:51:44 +0100

    lavede Opgave 1 A B C ----- tilføjede også lidt forebyggende kommentare

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,21 @@
 
 public class Main {
 
-    // TODO Din kode her
-
-    public String hello(String navn) {
-        return "";
+    public static void main(String[] args) {
+        System.out.println(hello("András"));
     }
+    // lidt bøvlet at bruge det der var i forvejen,
+    // blev nød til at lave hello om til en klasseVariabel, grundet at PSVM er static og "ikke-Statiske" metoder ikke
+    //kan blive kaldt i en static.
+
+    public static String hello(String navn){
+        String result = "hello";
+      if (navn.equals("András")) {
+          result = "Hello András";
+      }else if (navn.equals(""))
+      {result= "Hello World";}
+      return result;
+    }
+
 
 }
